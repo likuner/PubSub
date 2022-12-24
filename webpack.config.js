7bd4d12518a87de0e07f471a942caddd6683ccd3
likun <273676636@qq.com>
2022-12-24 11:42:44 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -2,35 +2,35 @@ const path = require('path')
 const { name } = require('./package.json')
 
 module.exports = (env) => {
-    const { prod } = env
-    return {
-        entry: './src/index.ts',
-        output: {
-            path: path.resolve(__dirname, 'dist'),
-            filename: 'index.min.js',
-            library: {
-                name: `${name.split('/').pop()}Lib`,
-                type: 'umd'
-            },
-            globalObject: 'this',
-            clean: true
-        },
-        mode: prod ? 'production' : 'development',
-        resolve: {
-            extensions: ['.ts', '.js', '.json']
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.ts$/,
-                    exclude: /node_modules/,
-                    use: [
-                        {
-                            loader: 'ts-loader'
-                        }
-                    ]
-                }
-            ]
+  const { prod } = env
+  return {
+    entry: './src/index.ts',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'index.min.js',
+      library: {
+        name: `${name.split('/').pop()}Lib`,
+        type: 'umd'
+      },
+      globalObject: 'this',
+      clean: true
+    },
+    mode: prod ? 'production' : 'development',
+    resolve: {
+      extensions: ['.ts', '.js', '.json']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'ts-loader'
+            }
+          ]
         }
+      ]
     }
+  }
 }
